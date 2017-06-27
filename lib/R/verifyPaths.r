7ee9ab754732dd3a0c78dd03764b25d8742d7ef4
Abhijit Dasgupta <dasgupab@mail.nih.gov>
2017-06-27 16:44:04 -0400

    Changed verifyPaths to a function

---
+++ lib/R/verifyPaths.r
@@ -0,0 +1,23 @@
+# Script to verify if there is current access to database directories and to 
+# identify the directories where the databases reside
+
+verifyPaths <- function() {
+  require(stringr)
+  if (Sys.info()['sysname'] == 'Windows') { # Work windows system
+    a <- system('wmic volume get name,label', intern = T)
+    ind1 <- which(unlist(lapply(a, str_detect, 'My Book')))
+    ind2 <- which(unlist(lapply(a, str_detect, 'ARAASTAT')))
+    if (length(c(ind1,ind2)) == 0) {
+      print('No access to databases')
+      return(NA)
+    }
+    if (length(ind1) > 0) {
+      drv <- str_split(a[[ind1]],'\\s{2,}')[[1]][2] # Priority for My Book
+    } else {
+      drv <- str_split(a[[ind2]], '\\s{2,}')[[1]][2]
+    }
+    
+    dbdir <- file.path(drv,'NIAMS','Ward','USRDS','Data','2015 data')
+    return(dbdir)
+  }
+}
