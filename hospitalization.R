950b68f609ecbece3423ede729640b8c42742a5e
Abhijit Dasgupta <aikidasgupta@gmail.com>
2017-10-19 15:30:51 -0400

    Kept only USRDS_ID, finished up to dementia

--- hospitalization.R
+++ hospitalization.R
@@ -10,53 +10,62 @@ dbs <- list(till2009, from2010)
 
 # Stroke ------------------------------------------------------------------
 
-stroke <- dbs %>% 
+stroke <- dbs %>%
   lapply(., function(db){
     db %>%
-      select(USRDS_ID, starts_with('HSDIAG'))%>% 
-      mutate(PRIM = substr(HSDIAG1,1,3)) %>% 
-      filter(PRIM == '430' | PRIM=='431' | PRIM == '432' | PRIM == '433' | PRIM=='434') %>% 
+      select(USRDS_ID, starts_with('HSDIAG'))%>%
+      mutate(PRIM = substr(HSDIAG1,1,3)) %>%
+      filter(PRIM == '430' | PRIM=='431' | PRIM == '432' | PRIM == '433' | PRIM=='434') %>%
       collect()
   })
 
-stroke_primary <- stroke %>% lapply(., function(db) db %>% select(USRDS_ID, HSDIAG1)) %>% bind_rows()
-
+stroke_primary <- stroke %>%
+  lapply(., function(db) db %>%  select(USRDS_ID)) %>% bind_rows() %>% distinct()
+head(stroke_primary)
 # Stroke with complications -----------------------------------------------
 
-stroke_compl <- stroke %>% 
+stroke_compl <- stroke %>%
   lapply(., function(db){
-    db %>% gather(diag, code, -USRDS_ID, -HSDIAG1) %>% 
-      mutate(code1 = substr(code,1,3)) %>% 
-      filter(code1 %in% c('438','342','344')) %>% 
-      select(USRDS_ID, HSDIAG1) %>% 
+    db %>% gather(diag, code, -USRDS_ID, -HSDIAG1) %>%
+      mutate(code1 = substr(code,1,3)) %>%
+      filter(code1 %in% c('438','342','344')) %>%
+      select(USRDS_ID) %>%
       distinct()
-  }) %>% 
-  bind_rows()
+  }) %>%
+  bind_rows() %>%
+  distinct()
 
+head(stroke_compl)
 
 # Lung cancer -------------------------------------------------------------
 
-LuCa <- dbs %>% 
+LuCa <- dbs %>%
   lapply(., function(db){
-    db %>% mutate(PRIM = substr(HSDIAG1,1,3)) %>% 
-      filter(PRIM=='162') %>% select(USRDS_ID, HSDIAG1) %>% collect()
-  }) %>% 
+    db %>% mutate(PRIM = substr(HSDIAG1,1,3)) %>%
+      filter(PRIM=='162') %>% select(USRDS_ID) %>% collect()
+  }) %>%
   bind_rows() %>% distinct()
-
+head(LuCa)
 # Metastatic cancer -------------------------------------------------------
 
-MetsCa <- dbs %>% 
+MetsCa <- dbs %>%
   lapply(., function(db){
-  db %>% mutate(PRIM=substr(HSDIAG1,1,3)) %>% 
-    filter(PRIM== '196' | PRIM == '197' | PRIM == '198' | PRIM == '199') %>% 
-    select(USRDS_ID, HSDIAG1) %>% collect()}) %>% 
-  bind_rows() %>% 
+  db %>% mutate(PRIM=substr(HSDIAG1,1,3)) %>%
+    filter(PRIM== '196' | PRIM == '197' | PRIM == '198' | PRIM == '199') %>%
+    select(USRDS_ID) %>% collect()}) %>%
+  bind_rows() %>%
   distinct()
-
+head(MetsCa)
 # Dementia ----------------------------------------------------------------
 
 ## I'm moving this to Python since it's much faster at processing the database
 ## row-wise. However, I need to get some SQL calls generated here.
 
-till2009 %>% select(USRDS_ID, starts_with('HSDIAG')) %>% show_query()
-from2010 %>% select(USRDS_ID, starts_with("HSDIAG")) %>% show_query()
+# till2009 %>% select(USRDS_ID, starts_with('HSDIAG')) %>% show_query()
+# from2010 %>% select(USRDS_ID, starts_with("HSDIAG")) %>% show_query()
+
+dementia <- read_csv('data/Dementia.csv')
+names(dementia) <- 'USRDS_ID'
+head(dementia)
+
+# Failure to thrive -------------------
