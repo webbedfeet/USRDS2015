1f3f830074a6bdf12a66244a6ac534bbb2e3f1ed
Abhijit Dasgupta <aikidasgupta@gmail.com>
2017-10-19 15:00:42 -0400

    Python code for dementia extraction

--- dementia.py
+++ dementia.py
@@ -21,7 +21,11 @@ c = con.cursor()
 # be the primary diagnosis
 
 ## till2009 extraction
+
+print("Starting 2009 extraction ...\n")
 dement_2009 = []
+
+
 c.execute('SELECT `USRDS_ID` AS `USRDS_ID`, `HSDIAG1` AS `HSDIAG1`, `HSDIAG2` AS `HSDIAG2`, `HSDIAG3` AS `HSDIAG3`, `HSDIAG4` AS `HSDIAG4`, `HSDIAG5` AS `HSDIAG5`, `HSDIAG6` AS `HSDIAG6`, `HSDIAG7` AS `HSDIAG7`, `HSDIAG8` AS `HSDIAG8`, `HSDIAG9` AS `HSDIAG9`, `HSDIAG10` AS `HSDIAG10` FROM `till2009`')
 for row in c:
     if any([re.search('^290', x) for x in row[1:]]):
@@ -40,9 +44,11 @@ for row in c:
         dement_2009.append(row[0])
         continue
 
+
 ## from 2010 extraction
+print("Starting 2010 extraction ...\n")
 dement_2010 = []
-c.execute('SELECT `USRDS_ID`,`HSDIAG1` AS `HSDIAG1`, `HSDIAG2` AS `HSDIAG2`, `HSDIAG3` AS `HSDIAG3`, `HSDIAG4` AS `HSDIAG4`, `HSDIAG5` AS `HSDIAG5`, `HSDIAG6` AS `HSDIAG6`, `HSDIAG7` AS `HSDIAG7`, `HSDIAG8` AS `HSDIAG8`, `HSDIAG9` AS `HSDIAG9`, `HSDIAG10` AS `HSDIAG10`, `HSDIAG11` AS `HSDIAG11`, `HSDIAG12` AS `HSDIAG12`, `HSDIAG13` AS `HSDIAG13`, `HSDIAG14` AS `HSDIAG14`, `HSDIAG15` AS `HSDIAG15`, `HSDIAG16` AS `HSDIAG16`, `HSDIAG17` AS `HSDIAG17`, `HSDIAG18` AS `HSDIAG18`, `HSDIAG19` AS `HSDIAG19`, `HSDIAG20` AS `HSDIAG20`, `HSDIAG21` AS `HSDIAG21`, `HSDIAG22` AS `HSDIAG22`, `HSDIAG23` AS `HSDIAG23`, `HSDIAG24` AS `HSDIAG24`, `HSDIAG25` AS `HSDIAG25`, `HSDIAG26` AS `HSDIAG26` FROM `from2010`')
+c.execute('SELECT `USRDS_ID`,`HSDIAG1` AS `HSDIAG1`, `HSDIAG2` AS `HSDIAG2`, `HSDIAG3` AS `HSDIAG3`, `HSDIAG4` AS `HSDIAG4`, `HSDIAG5` AS `HSDIAG5`, `HSDIAG6` AS `HSDIAG6`, `HSDIAG7` AS `HSDIAG7`, `HSDIAG8` AS `HSDIAG8`, `HSDIAG9` AS `HSDIAG9`, `HSDIAG10` AS `HSDIAG10`, `HSDIAG11` AS `HSDIAG11`, `HSDIAG12` AS `HSDIAG12`, `HSDIAG13` AS `HSDIAG13`, `HSDIAG14` AS `HSDIAG14`, `HSDIAG15` AS `HSDIAG15`, `HSDIAG16` AS `HSDIAG16`, `HSDIAG17` AS `HSDIAG17`, `HSDIAG18` AS `HSDIAG18`, `HSDIAG19` AS `HSDIAG19`, `HSDIAG20` AS `HSDIAG20`, `HSDIAG21` AS `HSDIAG21`, `HSDIAG22` AS `HSDIAG22`, `HSDIAG23` AS `HSDIAG23`, `HSDIAG24` AS `HSDIAG24`, `HSDIAG25` AS `HSDIAG25`, `HSDIAG26` AS `HSDIAG26` FROM `from2010` ')
 for row in c:
     if any([re.search('^290', x) for x in row[1:]]):
         dement_2010.append(row[0])
@@ -62,6 +68,10 @@ for row in c:
 
 con.close()
 
-import pandas as pd
-names = ['USRDS_ID']
-pd.DataFrame([dement_2009 + dement_2010], columns=names).drop_duplicates().to_csv('data/Dementia.csv', index=False)
+from more_itertools import unique_everseen
+
+out = list(unique_everseen(dement_2009+dement_2010))
+
+with open('data/Dementia.csv','w') as f:
+    for x in out :
+        f.write(str(int(x))+'\n')
