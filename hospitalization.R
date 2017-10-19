fd626085ebf274ce9264d56ed40af9e0c83f621d
Abhijit Dasgupta <aikidasgupta@gmail.com>
2017-10-19 11:39:49 -0400

    Updated hospitalization.R to combine extracts from both files in one go
    Also, extracted code for the dementia analysis for Python

--- hospitalization.R
+++ hospitalization.R
@@ -6,41 +6,57 @@ sql_conn = src_sqlite(file.path(dbdir,'USRDS.sqlite3'))
 
 till2009 <- tbl(sql_conn, 'till2009')
 from2010 <- tbl(sql_conn, 'from2010')
-
+dbs <- list(till2009, from2010)
 
 # Stroke ------------------------------------------------------------------
 
-stroke1 <- till2009 %>% 
-  select(USRDS_ID, starts_with('HSDIAG'))%>% 
-  mutate(PRIM = substr(HSDIAG1,1,3)) %>% 
-  filter(PRIM == '430' | PRIM=='431' | PRIM == '432' | PRIM == '433' | PRIM=='434') %>% 
-  collect()
-stroke2 <- from2010 %>% 
-  select(USRDS_ID, starts_with('HSDIAG')) %>% 
-  mutate(PRIM = substr(HSDIAG1,1,3)) %>% 
-  filter(PRIM == '430' | PRIM=='431' | PRIM == '432' | PRIM == '433' | PRIM=='434') %>% 
-  collect()
+stroke <- dbs %>% 
+  lapply(., function(db){
+    db %>%
+      select(USRDS_ID, starts_with('HSDIAG'))%>% 
+      mutate(PRIM = substr(HSDIAG1,1,3)) %>% 
+      filter(PRIM == '430' | PRIM=='431' | PRIM == '432' | PRIM == '433' | PRIM=='434') %>% 
+      collect()
+  })
 
-stroke_primary <- rbind(select(stroke1, USRDS_ID, HSDIAG1), select(stroke2, USRDS_ID, HSDIAG1)) %>% select(USRDS_ID) %>% distinct()
-
+stroke_primary <- stroke %>% lapply(., function(db) db %>% select(USRDS_ID, HSDIAG1)) %>% bind_rows()
 
 # Stroke with complications -----------------------------------------------
 
-stroke1_compl <- stroke1 %>% gather(diag, code, -USRDS_ID, -HSDIAG1) %>% 
-  mutate(code1 = substr(code,1,3)) %>% 
-  filter(code1 %in% c('438','342','344')) %>% 
-  select(USRDS_ID) %>% distinct()
-stroke2_compl <- stroke2 %>% gather(diag, code, -USRDS_ID, -HSDIAG1) %>% 
-  mutate(code1 = substr(code,1,3)) %>% 
-  filter(code1 %in% c('438','342','344')) %>% 
-  select(USRDS_ID) %>% distinct()
-stroke_compl <- rbind(stroke1_compl, stroke2_compl) %>% distinct()
+stroke_compl <- stroke %>% 
+  lapply(., function(db){
+    db %>% gather(diag, code, -USRDS_ID, -HSDIAG1) %>% 
+      mutate(code1 = substr(code,1,3)) %>% 
+      filter(code1 %in% c('438','342','344')) %>% 
+      select(USRDS_ID, HSDIAG1) %>% 
+      distinct()
+  }) %>% 
+  bind_rows()
 
 
 # Lung cancer -------------------------------------------------------------
 
-LuCa1 <- till2009 %>% mutate(PRIM = substr(HSDIAG1,1,3)) %>% 
-  filter(PRIM=='162') %>% select(USRDS_ID, HSDIAG1) %>% collect()
-LuCa2 <- from2010 %>% mutate(PRIM = substr(HSDIAG1,1,3)) %>% 
-  filter(PRIM=='162') %>% select(USRDS_ID, HSDIAG1) %>% collect()
-LuCa <- rbind(LuCa1, LuCa2)
+LuCa <- dbs %>% 
+  lapply(., function(db){
+    db %>% mutate(PRIM = substr(HSDIAG1,1,3)) %>% 
+      filter(PRIM=='162') %>% select(USRDS_ID, HSDIAG1) %>% collect()
+  }) %>% 
+  bind_rows() %>% distinct()
+
+# Metastatic cancer -------------------------------------------------------
+
+MetsCa <- dbs %>% 
+  lapply(., function(db){
+  db %>% mutate(PRIM=substr(HSDIAG1,1,3)) %>% 
+    filter(PRIM== '196' | PRIM == '197' | PRIM == '198' | PRIM == '199') %>% 
+    select(USRDS_ID, HSDIAG1) %>% collect()}) %>% 
+  bind_rows() %>% 
+  distinct()
+
+# Dementia ----------------------------------------------------------------
+
+## I'm moving this to Python since it's much faster at processing the database
+## row-wise. However, I need to get some SQL calls generated here.
+
+till2009 %>% select(USRDS_ID, starts_with('HSDIAG')) %>% show_query()
+from2010 %>% select(USRDS_ID, starts_with("HSDIAG")) %>% show_query()
