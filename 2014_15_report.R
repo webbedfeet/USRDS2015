7c4db22e9c509c3ba1465cbe13f8d5486d0083d9
Abhijit Dasgupta <aikidasgupta@gmail.com>
2017-11-29 17:46:46 -0500

    Updated 2014_15 report

--- 2014_15_report.R
+++ 2014_15_report.R
@@ -11,32 +11,35 @@
 #' in our study. However, there appear to be several discrepant observations between the 
 #' 2014 and 2015 datasets. 
 #' 
-#+ preamble, echo=FALSE, cache=TRUE, message=FALSE
+#+ preamble, echo=FALSE, cache=FALSE, message=FALSE
 library(tidyverse)
 ProjTemplate::reload()
 dbdir <- verifyPaths()
 dbdir14 <- verifyPaths(2014)
 con <- dbConnect(SQLite(), file.path(dbdir, 'USRDS.sqlite3'))
+con2 <- dbConnect(SQLite(), file.path(dbdir14, 'PR_db'))
+
+#+ setup, echo = FALSE, cache=TRUE, message=FALSE
 analytic_data <- readRDS('data/rda/analytic14.rds')
 
 P1 <- tbl(con, 'StudyIDs') %>% left_join(tbl(con, 'patients')) %>% 
   select(USRDS_ID, FIRST_SE, TX1DATE, DIED) %>% collect(n=Inf)
-P1 <- P1 %>% 
-  mutate_at(vars(FIRST_SE:DIED), as.Date)
+# P1 <- P1 %>% 
+#   mutate_at(vars(FIRST_SE:DIED), as.Date)
 analytic_data <- analytic_data %>% 
-  mutate_at(vars(FIRST_SE, TX1DATE, DIED), as.Date, origin = '1960-01-01')
+  mutate_at(vars(FIRST_SE, TX1DATE, DIED), as.Date, origin = '1960-01-01') %>% 
+  mutate_at(vars(FIRST_SE, TX1DATE, DIED), as.character)
 dbDisconnect(con)
 
 d <- analytic_data %>% select(USRDS_ID, FIRST_SE, TX1DATE, DIED) %>% 
   rename(FIRST_SE14 = FIRST_SE, TX1DATE14 = TX1DATE, DIED14 = DIED) %>% 
   left_join(P1 %>% rename(FIRST_SE15 = FIRST_SE, TX1DATE15 = TX1DATE, DIED15 = DIED))
 #'
-#' We find that there are `r sum(is.na(P1$FIRST_SE))` individuals who have FIRST_SE missing
-#' in the 2015 data who had this value in the 2014 data. Moreover, we see some disagreement
-#' in the values of this variable, as well. `r sum(d$FIRST_SE15 != d$FIRST_SE14, na.rm=T)`
-#' individuals have discrepant FIRST_SE observations, and 
-#' `r sum(d$FIRST_SE15 < '2003-01-01', na.rm=T)` 
-#' individuals now have FIRST_SE values before 2013, which was the cutoff used in the 2014 
-#' analysis.
+#' ### Incident dialysis (FIRST_SE)
+#' 1. There are `r d %>% filter(FIRST_SE14 != FIRST_SE15) %>% count()` individuals who have 
+#' discrepant first dialysis dates. Among them, there are `r d %>% filter(FIRST_SE15 < '2003-01-01') %>% count()` 
+#' records where initial dialysis was before 2013. These are listed below:
+#' 
+knitr::kable(d %>% filter(FIRST_SE15 < '2003-01-01') %>% arrange(FIRST_SE15))
 
 
