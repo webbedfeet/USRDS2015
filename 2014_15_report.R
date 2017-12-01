0de92115d6117e5e5054fd9f01e07d2f789cdb1f
Abhijit Dasgupta <dasgupab@mail.nih.gov>
2017-12-01 13:59:08 -0500

    Added info when FIRST_SE14 < DIED15

--- 2014_15_report.R
+++ 2014_15_report.R
@@ -12,20 +12,18 @@
 #' 2014 and 2015 datasets. 
 #' 
 #+ preamble, echo=FALSE, cache=FALSE, message=FALSE
-library(tidyverse)
 ProjTemplate::reload()
 dbdir <- verifyPaths()
 dbdir14 <- verifyPaths(2014)
 con <- dbConnect(SQLite(), file.path(dbdir, 'USRDS.sqlite3'))
 con2 <- dbConnect(SQLite(), file.path(dbdir14, 'PR_db'))
+knitr::opts_chunk$set(echo=FALSE)
 
 #+ setup, echo = FALSE, cache=TRUE, message=FALSE
 analytic_data <- readRDS('data/rda/analytic14.rds')
 
 P1 <- tbl(con, 'StudyIDs') %>% left_join(tbl(con, 'patients')) %>% 
-  select(USRDS_ID, FIRST_SE, TX1DATE, DIED) %>% collect(n=Inf)
-# P1 <- P1 %>% 
-#   mutate_at(vars(FIRST_SE:DIED), as.Date)
+  select(USRDS_ID, FIRST_SE, TX1DATE, DIED) %>% collect(n = Inf)
 analytic_data <- analytic_data %>% 
   mutate_at(vars(FIRST_SE, TX1DATE, DIED), as.Date, origin = '1960-01-01') %>% 
   mutate_at(vars(FIRST_SE, TX1DATE, DIED), as.character)
@@ -40,6 +38,16 @@ d <- analytic_data %>% select(USRDS_ID, FIRST_SE, TX1DATE, DIED) %>%
 #' discrepant first dialysis dates. Among them, there are `r d %>% filter(FIRST_SE15 < '2003-01-01') %>% count()` 
 #' records where initial dialysis was before 2013. These are listed below:
 #' 
-knitr::kable(d %>% filter(FIRST_SE15 < '2003-01-01') %>% arrange(FIRST_SE15))
+# knitr::kable(d %>% filter(FIRST_SE15 < '2003-01-01') %>% arrange(FIRST_SE15))
+#+ , echo=FALSE
+pander::pander(d %>% filter(FIRST_SE15 < '2003-01-01') %>% arrange(FIRST_SE15), style = 'rmarkdown', missing = '')
 
+#' 2. There are also 3 individuals where their dialysis date (FIRST_SE) in the 2014 database is after their death date in 
+#' the 2015 database
+pander::pander(d %>% filter(FIRST_SE14 > DIED15), missing='')
 
+#' ### Withdrawal times
+
+con <- dbConnect(SQLite(), file.path(dbdir, 'USRDS.sqlite3'))
+
+#' 
