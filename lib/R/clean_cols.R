70ab5c4e762c9d9b036ca6f0a2d36bda94c18906
Abhijit Dasgupta <aikidasgupta@gmail.com>
2018-06-16 00:04:06 -0400

    Added clean_cols and cox_snell

---
+++ lib/R/clean_cols.R
@@ -0,0 +1,13 @@
+clean_cols <- function(x, ..., arrange = F){
+  colv = quos(...)
+  x <- x %>% 
+    mutate_at(vars(!!!colv),as.character) # make sure columns are characters
+  if(arrange){
+    x <- x %>% arrange(!!!colv) # Ensure right order of columns
+  }
+  for(ii in rev(seq_along(colv))){
+    indx = duplicated(select(x, !!!colv[ii:1]))
+    x <- x %>% mutate_at(vars(!!colv[[ii]]), funs(ifelse(indx, '', .)))
+  }
+  return(x)
+}
---
+++ lib/R/cox_snell.R
@@ -0,0 +1,7 @@
+cox_snell <- function(mod, dat) {
+  cs <- -log(1 - pweibull(dat$time_from_event, shape = 1/mod$scale,
+                          scale = exp(predict(mod, dat, type='lp'))))
+  plot(survfit(cs, dat$cens_type==3)~1)
+  lines(qexp(ppoints(length(cs))), 1-ppoints(length(cs)), col='red')
+  return(cs)
+}
