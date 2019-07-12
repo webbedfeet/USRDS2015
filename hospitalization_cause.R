56780ff30f5faee1035e921153accc9ccfaca913
Abhijit Dasgupta <aikidasgupta@gmail.com>
2019-07-12 00:41:52 -0400

    Computed transplant frequencies

--- hospitalization_cause.R
+++ hospitalization_cause.R
@@ -666,3 +666,35 @@ openxlsx::write.xlsx(out, file = 'results/revision_JASN/WhiteModels.xlsx',
                      headerStyle = openxlsx::createStyle(textDecoration = 'BOLD') )
 
 
+##%######################################################%##
+#                                                          #
+####                  Transplantation                   ####
+#                                                          #
+##%######################################################%##
+
+# We've already computed a new survival time, surv_time2, that 
+# incorporates loss-to-followup, death and withdrawal only. So 
+# the clock doesn't stop for transplants. We'll see how this 
+# affects things.
+
+
+# Transplant frequency in each condition group ----------------------------
+
+hosp_post_dx <- readRDS(path(dropdir, 'revision_JASN', 'final_hosp_data.rds'))
+
+tx_freq <- map(hosp_post_dx, 
+               ~count(., cens_type) %>% 
+                 mutate(Percentage = 100*n/sum(n)) %>% 
+                 select(-n) %>% 
+                 filter(cens_type == 2)) %>% 
+  bind_rows(.id = 'Condition') %>% 
+  mutate(Condition = condition_code[Condition])
+
+
+# Crude & adjusted models for discontinuation -----------------------------
+
+
+
+# Crude & adjusted models for mortality -----------------------------------
+
+
