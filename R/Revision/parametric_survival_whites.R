ab30cd4b7590110def8496112c157b3ca218b8bd
Abhijit Dasgupta <aikidasgupta@gmail.com>
2019-07-30 15:31:19 -0400

    added disgprc, started comorbidity index

--- R/Revision/parametric_survival_whites.R
+++ R/Revision/parametric_survival_whites.R
@@ -9,6 +9,13 @@ reload()
 dropdir <- path(find_dropbox(), 'NIAMS','Ward','USRDS2015','data')
 analytic_data <- read_fst(path(dropdir,'Analytic.fst'))
 
+dbdir <- verifyPaths()
+sql_conn <- dbConnect(SQLite(), file.path(dbdir,'USRDS.sqlite3'))
+
+till2009 <- tbl(sql_conn, 'till2009')
+from2010 <- tbl(sql_conn,'from2010')
+studyids <- tbl(sql_conn, 'StudyIDs')
+
 
 # Fixing the REGION specification -----------------------------------------
 
@@ -37,6 +44,57 @@ analytic_whites <- analytic_whites %>%
     cens_type == 2 ~ "Transplant",
     cens_type == 3 ~ "Discontinued",
     TRUE ~ NA_character_)) # NA of type character
+
+# Adding DISGRPC codes to analytic data --------------------------------------------
+
+disgrpc_code <- readRDS(path(dropdir, 'disgrpc_code.rds')) # See cause_esrd.R
+disgrpc <- fst(path(dropdir, 'raw_data.fst'))[,c('USRDS_ID','DISGRPC')] %>%
+  distinct() %>%
+  left_join(disgrpc_code, by=c('DISGRPC'='Format')) %>%
+  mutate(Description = ifelse(Description %in% c('8','**OTHER**'), NA, Description)) %>%
+  mutate(Description = fct_other(Description,keep=c('Diabetes','Hypertension','Glomeruloneph.'))) %>%
+  rename(ESRD_Cause = Description)
+analytic_whites <- analytic_whites %>% left_join(disgrpc) # Adding reason for dialysis
+
+
+# Adding comorbidity index ------------------------------------------------
+comorb_codes <- list(
+  'ASHD' = '410-414, V4581, V4582',
+  'CHF' = '39891, 422, 425, 428, 402X1,404X1, 404X3, V421',
+  'CVATIA' = '430-438',
+  'PVD' = '440-444, 447, 451-453, 557',
+  'Other cardiac' = '420-421, 423-424, 429, 7850-7853,V422,V433',
+  'COPD' = '491-494, 496, 510',
+  'GI Bleeding' = '4560-4562, 5307, 531-534, 56984, 56985,578',
+  'Liver' = '570-571,5721, 5724,5731-5733,V427',
+  'Dysrhhythmia' = '426-427,V450, V533',
+  'Cancer' = '140-172, 174-208, 230-231, 233-234',
+  'Diabetes' = '250, 3572, 3620X, 36641'
+) %>% map(icd9_codes)
+
+determine_comorbs <- function(d){
+  d %>% select(USRDS_ID,starts_with("CLM"), starts_with("HSDIAG")) %>% 
+    gather(DIAG, codes, starts_with("HSDIAG")) %>% 
+    bind_cols(as.data.frame(lapply(comorb_codes, function(x) .$codes %in% x))) %>% 
+    select(-DIAG, -codes) %>% 
+    group_by(USRDS_ID, CLM_FROM,CLM_THRU) %>% 
+    summarise_all(any) %>% 
+    ungroup()
+}
+
+d1 <- till2009 %>% 
+  select(USRDS_ID, starts_with("CLM"), starts_with("HSDIAG")) %>% 
+  collect(n=Inf) %>% 
+  semi_join(select(analytic_whites, 'USRDS_ID'))
+
+d2 <- from2010 %>% 
+  select(USRDS_ID, starts_with("CLM"), starts_with("HSDIAG")) %>% 
+  collect(n = Inf) %>% 
+  semi_join(select(analytic_whites, 'USRDS_ID'))
+
+d <- bind_rows(d1,d2)
+
+u <- determine_comorbs(d)
 
 write_fst(analytic_whites, path(dropdir, 'Analytic_Whites.fst'))
 
