9df1a933571672972629726eb231484f985011b9
Abhijit Dasgupta <dasgupab@mail.nih.gov>
2019-07-31 17:42:01 -0400

    Cleaning albumin data

--- R/Revision/parametric_survival_whites.R
+++ R/Revision/parametric_survival_whites.R
@@ -6,7 +6,7 @@
 abhiR::reload()
 
 dropdir <- path(find_dropbox(), 'NIAMS','Ward','USRDS2015','data')
-# analytic_data <- read_fst(path(dropdir,'Analytic.fst'))
+analytic_data <- read_fst(path(dropdir,'Analytic.fst'))
 
 dbdir <- verifyPaths()
 sql_conn <- dbConnect(SQLite(), file.path(dbdir,'USRDS.sqlite3'))
@@ -37,6 +37,17 @@ analytic_data <- analytic_data %>%
   ))
 
 
+# Adding DISGRPC codes to analytic data --------------------------------------------
+
+disgrpc_code <- readRDS(path(dropdir, 'disgrpc_code.rds')) # See cause_esrd.R
+disgrpc <- fst(path(dropdir, 'raw_data.fst'))[,c('USRDS_ID','DISGRPC')] %>%
+  distinct() %>%
+  left_join(disgrpc_code, by=c('DISGRPC'='Format')) %>%
+  mutate(Description = ifelse(Description %in% c('8','**OTHER**'), NA, Description)) %>%
+  mutate(Description = fct_other(Description,keep=c('Diabetes','Hypertension','Glomeruloneph.'))) %>%
+  rename(ESRD_Cause = Description) 
+analytic_data <- analytic_data %>% left_join(disgrpc) # Adding reason for dialysis
+
 # Create white subset -----------------------------------------------------
 
 analytic_whites <- analytic_data %>% filter(RACE2 == 'White')
@@ -49,149 +60,25 @@ analytic_whites <- analytic_whites %>%
     cens_type == 3 ~ "Discontinued",
     TRUE ~ NA_character_)) # NA of type character
 
-# Adding DISGRPC codes to analytic data --------------------------------------------
+# Adding comorbidities ------------------------------------------------
+## We wil add albumin, alcohol use and drug use to the set of comorbidities,
+## and we'll utilize BMI and Smoking which are already included
 
-disgrpc_code <- readRDS(path(dropdir, 'disgrpc_code.rds')) # See cause_esrd.R
-disgrpc <- fst(path(dropdir, 'raw_data.fst'))[,c('USRDS_ID','DISGRPC')] %>%
-  distinct() %>%
-  left_join(disgrpc_code, by=c('DISGRPC'='Format')) %>%
-  mutate(Description = ifelse(Description %in% c('8','**OTHER**'), NA, Description)) %>%
-  mutate(Description = fct_other(Description,keep=c('Diabetes','Hypertension','Glomeruloneph.'))) %>%
-  rename(ESRD_Cause = Description)
-analytic_whites <- analytic_whites %>% left_join(disgrpc) # Adding reason for dialysis
+medevid <- studyids %>% left_join(tbl(sql_conn, 'medevid')) %>% 
+  select(USRDS_ID, ALBUM, ALBUMDT, ALBUMLM,ALCOH, DRUG) %>% 
+  collect(n = Inf) %>% 
+  mutate_at(vars(ALCOH, DRUG), ~ifelse(. == '', NA, .))
+medevid1 <- medevid %>% left_join(analytic_data %>% select(USRDS_ID, FIRST_SE)) %>% 
+  filter_at(vars(ALCOH:DRUG), ~!is.na(.)) %>% 
+  mutate(ALBUMDT = as.Date(ALBUMDT),
+         time_from_se = abs(FIRST_SE - ALBUMDT))
+bl=filter(medevid1, !is.na(ALBUM))
+bl %>% count(USRDS_ID) %>% filter(n > 1) %>% left_join(bl) -> bl2
+bl2 %>% group_by(USRDS_ID) %>% 
+  filter(time_from_se == min(time_from_se, na.rm=T)) %>% 
+  ungroup() %>% 
+  distinct() -> bl3
 
-
-# Adding comorbidity index ------------------------------------------------
-comorb_codes <- list(
-  'ASHD' = '410-414, V4581, V4582',
-  'CHF' = '39891, 422, 425, 428, 402X1,404X1, 404X3, V421',
-  'CVATIA' = '430-438',
-  'PVD' = '440-444, 447, 451-453, 557',
-  'Other.cardiac' = '420-421, 423-424, 429, 7850-7853,V422,V433',
-  'COPD' = '491-494, 496, 510',
-  'GI.Bleeding' = '4560-4562, 5307, 531-534, 56984, 56985,578',
-  'Liver' = '570-571,5721, 5724,5731-5733,V427',
-  'Dysrhhythmia' = '426-427,V450, V533',
-  'Cancer' = '140-172, 174-208, 230-231, 233-234',
-  'Diabetes' = '250, 3572, 3620X, 36641'
-) %>% map(icd9_codes)
-
-determine_comorbs <- function(d){
-  d %>% select(USRDS_ID,starts_with("CLM"), starts_with("HSDIAG")) %>% 
-    gather(DIAG, codes, starts_with("HSDIAG")) %>% 
-    bind_cols(as.data.frame(lapply(comorb_codes, function(x) .$codes %in% x))) %>% 
-    select(-DIAG, -codes) %>% 
-    group_by(USRDS_ID, CLM_FROM,CLM_THRU) %>% 
-    summarise_all(any) %>% 
-    ungroup()
-}
-
-white <- analytic %>% filter(RACE2=='White') %>% select(USRDS_ID)
-d1 <- till2009 %>% select(USRDS_ID, starts_with("CLM"), starts_with("HSDIAG")) %>% 
-  inner_join(white) %>% collect(n = Inf)
-d2 <- from2010 %>% select(USRDS_ID, starts_with("CLM"), starts_with("HSDIAG")) %>% 
-  inner_join(white) %>% collect(n=Inf)
-library(data.table)
-d <- as.data.table(bind_rows(d1,d2)); rm(d1, d2); gc()
-
-impute_ones <- function(x){
-  if(any(x)){
-    n <- length(x)
-    i <- min(which(x)) # Assumes T/F variable
-    x[i:n] <- TRUE
-    return(x)
-  } else{
-    return(x)
-  }
-}
-u <- d[USRDS_ID %in% c(3892, 27132, 50154)]
-
-
-
-ids <- unique(d$USRDS_ID)
-grps <- sample(1:5, length(ids), replace=T)
-
-d2 <- d[USRDS_ID %in% ids[grps==1]]
-
-
-f2 <- function(d2){
-  d21 <- melt(d2, c('USRDS_ID','CLM_FROM','CLM_THRU'), 
-              variable.name = 'DIAG', value.name = 'codes')
-  bl <- d21[,lapply(comorb_codes, f, codes)]
-  d21 <- cbind(d21[,c('USRDS_ID','CLM_FROM','CLM_THRU')], bl)
-  d31 <- d21[, lapply(.SD, any, na.rm=T), by=.(USRDS_ID, CLM_FROM, CLM_THRU)]
-  d31 <- d31[, lapply(.SD, impute_ones), by=.(USRDS_ID), .SDcols = names(comorb_codes)]
-  d31[,comorb_indx := ASHD + 3*CHF + 
-        2 * (CVATIA + PVD + Other.cardiac + COPD + 
-               GI.Bleeding + Liver + Dysrhhythmia + Cancer) +
-        Diabetes]
-  d41 <- d31[,.(comorb_indx=max(comorb_indx)), by=.(USRDS_ID)]
-  return(d41)
-}
-
-
-f1 <- function(u){
-  u1 <- melt(u, c('CLM_FROM','CLM_THRU'),
-             variable.name = 'DIAG', value.name = 'codes')
-  bl <- u1[,lapply(comorb_codes, f, codes)]
-  u1 <- cbind(u1[,c('CLM_FROM','CLM_THRU')], bl)
-  u2 <- u1[,lapply(.SD, any, na.rm=T), by=c('CLM_FROM','CLM_THRU')]
-  u3 <- u2[,lapply(.SD, impute_ones), .SDcols = names(comorb_codes)]
-  u3[,comorb_index := ASHD + 3*CHF + 
-       2 * (CVATIA + PVD + Other.cardiac + COPD + 
-              GI.Bleeding + Liver + Dysrhhythmia + Cancer) +
-       Diabetes]
-  out <- u3[, .(comorb_index = max(comorb_index))]
-  return(out)
-}
-
-D <- d[,f1(.SD), by=.(USRDS_ID)]
-h()
-f <- function(x,y) y %in% x
-f1 <- function(d){
-  d1 <- d %>% gather(DIAG, codes, starts_with("HSDIAG")) 
-  bl <- map_dfc(comorb_codes, ~D$codes %in% .)
-  d1 <- cbind(select(d1, USRDS_ID:CLM_THRU), bl) %>% 
-    group_by(vars(USRDS_ID:CLM_THRU)) %>% 
-    summarize_all(any)
-  return(d1)
-}
-
-tst <- d %>% split(., .$USRDS_ID) %>% 
-  map_df(f1)
-  
-  d1 <- melt(d, c('USRDS_ID','CLM_FROM','CLM_THRU'), 
-             variable.name='DIAG',value.name='codes')
-  bl <- d1[,lapply(comorb_codes, f, codes )]
-  d1 <- cbind(d1[,c("USRDS_ID",'CLM_FROM','CLM_THRU')], bl)
-  d2 <- d1[, ]
-}
-
-
-
-D <- melt(d, c('USRDS_ID','CLM_FROM','CLM_THRU'), variable.name = 'DIAG',
-          value.name = 'codes')
-blah <- D[,lapply(comorb_codes, f, codes)]
-D <- cbind(D[,c("USRDS_ID",'CLM_FROM','CLM_THRU')], blah)
-
-D[,"ASHD" := codes %in% comorb_codes[['ASHD']]]
-D <- d %>% gather(DIAG, codes, starts_with("HSDIAG"))
-blah <- map_dfc(comorb_codes, ~D$codes %in% .x)
-D <- bind_cols(D, blah) %>% select(-DIAG, -codes) %>% 
-  group_by(vars(USRDS_ID:CLM_THRU)) %>% 
-  summarize_all(any) %>% 
-  ungroup(); rm(blah); gc()
-
-for(n in names(comorb_codes)){
-  D[,n] <- D$codes %in% comorb_codes[[n]]
-}
-
-blah <- map_dfc(comorb_codes, ~D$codes %in% .x) %>% bind_cols(D$USRDS_ID)
-write_fst(d, 'tmp_hosp.fst')
-
-# length(setdiff(analytic_data$USRDS_ID, d$USRDS_ID))/length(unique(analytic_data$USRDS_ID))
-
-u <- determine_comorbs(d)
 
 write_fst(analytic_whites, path(dropdir, 'Analytic_Whites.fst'))
 
