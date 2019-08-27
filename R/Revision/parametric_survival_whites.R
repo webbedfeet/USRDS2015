4a819a307d1543102e07c3a2ea76ad7fdafdc3c2
Abhijit Dasgupta <dasgupab@mail.nih.gov>
2019-08-27 13:06:37 -0400

    Updated psm and cox-snell definitions

--- R/Revision/parametric_survival_whites.R
+++ R/Revision/parametric_survival_whites.R
@@ -6,59 +6,57 @@
 abhiR::reload()
 dropdir <- path(find_dropbox(), 'NIAMS','Ward','USRDS2015','data')
 
-analytic_whites <- read_fst(path(dropdir, 'Analytic_Whites.fst'))
+analytic_whites <- read_fst(path(dropdir, 'Analytic_Whites.fst')) %>% 
+  mutate(REGION = factor(REGION))
 analytic_whites_byagegrp <- split(analytic_whites, analytic_whites$AGEGRP)
 
-model_template <- function(dat){
+model_template <- function(dat, dist = 'weibull'){
   require(rms)
   m1 <- survreg(Surv(surv_time + 0.01, cens_type==3) ~ 
-              SEX + factor(REGION) + rcs(zscore) + 
-              ESRD_Cause +  
-              Cancer + Cardia + Cva + Hyper + Ihd + Pulmon + Pvasc + Smoke + 
-              DIABETES + ALCOH + DRUG + BMI2, 
-            data = dat, dist='weibull')
-  return(m1)
-}
-
-model_template2 <- function(dat){
-  require(rms)
-  m1 <- coxph(Surv(surv_time + 0.1, cens_type==3) ~ 
-                  SEX + factor(REGION) + rcs(zscore) + 
-                  ESRD_Cause +  
+                  factor(REGION) + rcs(zscore) + 
+                  SEX*(ESRD_Cause +  BMI2) + 
                   Cancer + Cardia + Cva + Hyper + Ihd + Pulmon + Pvasc + Smoke + 
                   DIABETES + ALCOH + DRUG + BMI2, 
-                data = dat)
+                data = dat, dist=dist)
+  return(m1)
+}
+
+model_template_rms <- function(dat, dist = 'weibull'){
+  require(rms)
+  m1 <- psm(Surv(surv_time + 0.01, cens_type==3) ~ 
+                  REGION + SEX*rcs(zscore) + 
+                  SEX*(ESRD_Cause +  BMI2) + 
+                  Cancer + Cardia + Cva + Hyper + Ihd + Pulmon + Pvasc + Smoke + 
+                  DIABETES + ALCOH + DRUG + BMI2, 
+                data = dat, dist=dist, y = T, x = T)
   return(m1)
 }
 
 
-whites_models_p <- map(analytic_whites_byagegrp, model_template)
-whites_models <- map(analytic_whites_byagegrp, model_template2)
+whites_models_weibull <- map(analytic_whites_byagegrp, model_template_rms)
+whites_models_loglogistic <- map(analytic_whites_byagegrp, model_template_rms, dist = 'loglogistic')
+whites_models_lognormal <- map(analytic_whites_byagegrp, model_template_rms, dist = 'lognormal')
 
-
-CoxSnell <- function(m){
-  times <- as.matrix(m$y)[,1]
-  status <- as.matrix(m$y)[,2]
-  cs <- -log(1-pweibull(times, shape = 1/m$scale, 
-                        scale = exp(predict(m, type = 'lp'))))
-  plot(survfit(Surv(cs, status)~1))
-  lines(qexp(ppoints(length(cs))), 1- ppoints(length(cs)), col='red')
-  return(cs)
+CoxSnell.psm <- function(m, plot = F){
+  require(rms)
+  if(!is(m, 'psm')) stop("Model needs to be run using rms::psm")
+  r <- residuals(m)
+  cs <- -log(attr(r,'theoretical')(exp(r[,1]))) # exp for weibull, loglogistic and lognormal
+  if(plot){
+    survd <- broom::tidy(survfit(Surv(cs, r[,2])~1)) # Compute survival curve of censored CS resid
+    ggplot(survd, aes(time, -log(estimate)))+geom_point() + geom_abline(color = 'red')
+  }
+  return(Surv(cs, r[,2]))
 }
 
-CoxSnell2 <- function(m){
-  linFit = predict(m, type='lp')
-  times = as.matrix(m$y)[,1]
-  sderr = (log(times) - linFit)/m$scale
-  csr = -log(exp(-exp(sderr)))
-  return(csr)
-}
+cs_weibull <- map(whites_models_weibull, CoxSnell.psm)
+cs_loglogistic <- map(whites_models_loglogistic, CoxSnell.psm)
+cs_lognormal <- map(whites_models_lognormal, CoxSnell.psm)
 
-CoxSnellPH <- function(m){
-  status <- as.matrix(m$y)[,2]
-  cs = status - resid(m, type='martingale')
-  plot(survfit(Surv(cs, status)~1))
-  lines(qexp(ppoints(length(cs))), 1-ppoints(length(cs)), col='red')
-  return(cs)
+plot_cs <- function(cs){
+  require(rms)
+  require(broom)
+  d <- tidy(npsurv(cs~1))
+  plt <- ggplot(d, aes(time, -log(estimate)))+geom_point() + geom_abline(color='red')
   
 }
