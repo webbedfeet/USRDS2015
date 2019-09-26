65870399eca2ec9740670da13b95f40caeee8ffe
Abhijit Dasgupta <aikidasgupta@gmail.com>
2019-09-26 10:55:43 -0400

    Update normalize_dichot
    Was missing the case where (a) unique non-missing value, and (b) option where if Y=N then prefer Y

--- lib/R/normalize_dichot.R
+++ lib/R/normalize_dichot.R
@@ -5,19 +5,25 @@
 #' If the observed values have the same frequency, or they are all missing, the values will be 
 #' replaced by NA
 #'
-#' @param x A character vector with outcomes denoted by "Y" and "N"
+#' @param x A character vector with outcomes denoted by "Y" and "N". Any "" are converted to NA first
+#' @param prefer.y If Y and N are equal frequencies, choose Y. Otherwise, make NA
 #'
 #' @return A vector with either all missing values or the modal value observed
 #' @export
 #'
 #' @examples
-normalize_dichot <- function(x) {
-  if (length(unique(x))==1) {
-    return(x) # Only one unique value
+normalize_dichot <- function(x, prefer.y=T) {
+  x[x==''] <- NA
+  if (length(unique(x[!is.na(x)]))==1) {
+    return(rep(unique(x[!is.na(x)]), length(x))) # Only one unique value
   }
   t1 <- table(x)
   if (length(unique(t1))==1) {
+    if(prefer.y){
+      return(rep("Y", length(x)))
+    } else{
     return(as.character(rep(NA, length(x)))) # Both Y and N have same frequency
+    }
   }
   prop_y <- mean(x == "Y", na.rm = T)
   if (prop_y > 0.5){
