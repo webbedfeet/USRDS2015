e6a0e4f64153b216153767e7c15465b0bb210a5f
Abhijit Dasgupta <dasgupab@mail.nih.gov>
2019-11-29 10:46:49 -0500

    First pass at updating cox_snell
    Still not matching hand computation

--- lib/R/cox_snell.R
+++ lib/R/cox_snell.R
@@ -1,3 +1,15 @@
+CoxSnell <- function(mod){
+  UseMethod("CoxSnell", mod)
+}
+
+CoxSnell.psm <- function(mod){
+  require(rms)
+  if(!is(mod, 'psm')) stop("Model needs to be run using rms::psm")
+  if(length(mod$y)==0) stop('Model needs to be run with option y=TRUE')
+  cs <- -log(Survival(mod)(mod$y[,1], mod$linear.predictor, mod$scale))
+  return(Surv(cs, mod$y[,2]))
+}
+
 cox_snell <- function(mod, dat) {
   cs <- -log(1 - pweibull(dat$time_from_event, shape = 1/mod$scale,
                           scale = exp(predict(mod, dat, type='lp'))))
