c30f8e58ab0b74316fa9898f7e1ba16766d532b3
Dasgupta <dasgupab@nih.gov>
2019-11-29 14:59:30 -0500

    Finalized discontinuation models

--- R/Revision/parametric_survival_whites.R
+++ R/Revision/parametric_survival_whites.R
@@ -18,26 +18,27 @@ analytic_whites_byagegrp <- split(analytic_whites, analytic_whites$AGEGRP)
 
 # TODO: Update this with comorbidity index from AnalyticUpdated.fst
 # The current method misses liver disease and GI bleeding
-model_template <- function(dat, dist = 'weibull'){
-  require(rms)
-  m1 <- survreg(Surv(surv_time + 0.01, cens_type==3) ~
-                  factor(REGION) + SEX*rcs(zscore) +
-                  SEX*(ESRD_Cause +  BMI2) +
-                  comorb_indx +
-                  # Cancer + Cardia + Cva + Hyper + Ihd + Pulmon + Pvasc + Smoke +
-                  DIABETES + ALCOH + DRUG ,
-                data = dat, dist=dist)
-  return(m1)
-}
+# model_template <- function(dat, dist = 'weibull'){
+#   require(rms)
+#   m1 <- survreg(Surv(surv_time + 0.01, cens_type==3) ~
+#                   factor(REGION) + SEX*rcs(zscore) +
+#                   SEX*(ESRD_Cause +  BMI2) +
+#                   comorb_indx +
+#                   # Cancer + Cardia + Cva + Hyper + Ihd + Pulmon + Pvasc + Smoke +
+#                   DIABETES + ALCOH + DRUG ,
+#                 data = dat, dist=dist)
+#   return(m1)
+# }
 
 model_template_rms <- function(dat, dist = 'weibull'){
   require(rms)
   m1 <- psm(Surv(surv_time + 0.01, cens_type==3) ~
-              REGION + SEX + zscore +
+              REGION + SEX + rcs(zscore) +
               ESRD_Cause +
               rcs(comorb_indx) +
               # Cancer + Cardia + Cva + Hyper + Ihd + Pulmon + Pvasc + Smoke +
               DIABETES + ALCOH + DRUG + BMI2 +
+              SEX:DIABETES + SEX:ALCOH + SEX:DRUG +
               SEX:BMI2 + SEX:ESRD_Cause + SEX:REGION +
               SEX*rcs(comorb_indx),
                 data = dat, dist=dist, y = T, x = T)
@@ -61,17 +62,17 @@ whites_models_lognormal <- map(analytic_whites_byagegrp, model_template_rms, dis
 #   return(Surv(cs, r[,2]))
 # }
 
-cs_weibull <- map(whites_models_weibull, CoxSnell.psm)
-cs_loglogistic <- map(whites_models_loglogistic, CoxSnell.psm)
-cs_lognormal <- map(whites_models_lognormal, CoxSnell.psm)
+cs_weibull <- map(whites_models_weibull, CoxSnell)
+cs_loglogistic <- map(whites_models_loglogistic, CoxSnell)
+cs_lognormal <- map(whites_models_lognormal, CoxSnell)
 
 plot_cs <- function(cs){
   require(rms)
   require(broom)
   d <- tidy(npsurv(cs~1))
-  plt <- ggplot(d, aes(time, -log(estimate)))+geom_point() +
+  plt <- ggplot(d, aes(time, -log(estimate)))+geom_line()+#geom_point(size=1) +
     geom_abline(color='red') +
-    labs(x = 'Time', y = "Cumulative Hazard")
+    labs(x = 'Cox-Snell Residuals', y = "Cumulative Hazard")
   return(plt)
 }
 
@@ -79,19 +80,26 @@ plot_disc_weib <- map(cs_weibull, plot_cs)
 plot_disc_ln <- map(cs_lognormal, plot_cs)
 plot_disc_ll <- map(cs_loglogistic, plot_cs)
 
+pdf('graphs/Revision/CSExplore_discontinuation.pdf')
+for(i in 1:6){
+  print(cowplot::plot_grid(plot_disc_weib[[i]], plot_disc_ln[[i]],plot_disc_ll[[i]], nrow=2))
+}
+dev.off()
+
+
 #' Comparing all the models, we have the following models for particular
 #' age groups:
 #'
-#' Weibull: 1,2,6
-#' Lognormal: 3, 4
-#' Loglogistic: 5
+#' Weibull: 1,5,6
+#' Lognormal: 3
+#' Loglogistic: 2,4
 
 final_models_disc <- list(
   whites_models_weibull[[1]],
-  whites_models_weibull[[2]],
+  whites_models_loglogistic[[2]],
   whites_models_lognormal[[3]],
-  whites_models_lognormal[[4]],
-  whites_models_loglogistic[[5]],
+  whites_models_loglogistic[[4]],
+  whites_models_weibull[[5]],
   whites_models_weibull[[6]]
 )
 names(final_models_disc) <- names(whites_models_weibull)
@@ -108,26 +116,29 @@ names(final_models_disc) <- names(whites_models_weibull)
 plot(survfit(Surv(surv_time, cens_type==2)~AGEGRP, data = analytic_whites))
 ## This appears to show an exponential or weibull-type model will work better
 
-model_template_transplants <- function(dat, dist = 'weibull'){
-  require(rms)
-  m1 <- survreg(Surv(surv_time + 0.01, cens_type==2) ~
-                  factor(REGION) + SEX + rcs(zscore) +
-                  ESRD_Cause +
-                  rcs(comorb_indx) +
-                  # Cancer + Cardia + Cva + Hyper + Ihd + Pulmon + Pvasc + Smoke +
-                  DIABETES + ALCOH + DRUG + BMI2,
-                data = dat, dist=dist)
-  return(m1)
-}
+# model_template_transplants <- function(dat, dist = 'weibull'){
+#   require(rms)
+#   m1 <- survreg(Surv(surv_time + 0.01, cens_type==2) ~
+#                   factor(REGION) + SEX + rcs(zscore) +
+#                   ESRD_Cause +
+#                   rcs(comorb_indx) +
+#                   # Cancer + Cardia + Cva + Hyper + Ihd + Pulmon + Pvasc + Smoke +
+#                   DIABETES + ALCOH + DRUG + BMI2,
+#                 data = dat, dist=dist)
+#   return(m1)
+# }
 
 model_template_transplants_rms <- function(dat, dist = 'weibull'){
   library(rms)
   m1 <- psm(Surv(surv_time + 0.01, cens_type==2) ~
-              REGION + SEX+ zscore +
-              ESRD_Cause  +
+              REGION + SEX + rcs(zscore) +
+              ESRD_Cause +
               rcs(comorb_indx) +
               # Cancer + Cardia + Cva + Hyper + Ihd + Pulmon + Pvasc + Smoke +
-              DIABETES + ALCOH + DRUG + BMI2,
+              DIABETES + ALCOH + DRUG + BMI2 +
+              SEX:DIABETES + SEX:ALCOH + SEX:DRUG +
+              SEX:BMI2 + SEX:ESRD_Cause + SEX:REGION +
+              SEX*rcs(comorb_indx),
             data = dat, dist=dist, y = T, x = T)
   return(m1)
 }
@@ -147,6 +158,12 @@ cs_ln <- map(models1_ln, CoxSnell.psm)
 plots_tr_weib <- map(cs_weib, plot_cs)
 plots_tr_ll <- map(cs_ll, plot_cs)
 plots_tr_ln <- map(cs_ln, plot_cs)
+
+pdf('graphs/Revision/CSExplore_transplant.pdf')
+for(i in 1:6){
+  print(cowplot::plot_grid(plot_tr_weib[[i]], plot_tr_ln[[i]],plot_tr_ll[[i]], nrow=2))
+}
+dev.off()
 
 #' Comparing all the models, we have the following models for particular
 #' age groups:
